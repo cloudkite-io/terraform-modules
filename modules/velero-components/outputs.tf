1021c8ee770d3d82c0d706bd44af1e4dfcabc9d0
Eduar Tua <eduartua@gmail.com>
2019-10-11 00:24:35 -0500

    rename velero sa module to velero-components and add backups bucket resource

--- modules/velero-components/outputs.tf
+++ modules/velero-components/outputs.tf
@@ -2,3 +2,7 @@ output "email" {
   description = "The email address of the custom gke service account."
   value       = google_service_account.velero_service_account.email
 }
+
+output "velero-sa-keyfile" {
+  value = google_service_account_key.velero.private_key
+}
