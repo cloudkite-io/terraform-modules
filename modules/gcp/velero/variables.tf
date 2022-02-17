d1de2cb2dc980a42faa3e96056d83271a8203e60
KoomeKiriinya <ian@cloudkite.io>
2022-02-17 20:51:28 +0300

    ensure backup bucket is in different project

--- modules/gcp/velero/variables.tf
+++ modules/gcp/velero/variables.tf
@@ -3,6 +3,11 @@ variable "project" {
   type        = string
 }
 
+variable "backup_project" {
+  description = "The name of the GCP Project where all backups will be stored."
+  type        = string
+}
+
 variable "service_account_name" {
   description = "The name of the custom service account. This parameter is limited to a maximum of 28 characters."
   type        = string
