8773d1e9f55c3568128a738c798f0d23c666befa
KoomeKiriinya <ian@cloudkite.io>
2024-02-28 16:26:59 +0300

    adding other arguments to security rules

--- modules/azure/networking/main.tf
+++ modules/azure/networking/main.tf
@@ -71,19 +71,19 @@ resource "azurerm_network_security_group" "security_groups" {
     for_each = each.value.security_rules
 
     content {
-      name                       = security_rule.key
-      priority                   = security_rule.value.priority
-      direction                  = security_rule.value.direction
-      access                     = security_rule.value.access
-      protocol                   = security_rule.value.protocol
-      source_port_range          = security_rule.value.source_port_range
-      source_port_ranges          = security_rule.value.source_port_ranges
-      destination_port_range     = security_rule.value.destination_port_range
-      destination_port_ranges     = security_rule.value.destination_port_ranges
-      source_address_prefix      = security_rule.value.source_address_prefix
-      source_address_prefixes      = security_rule.value.source_address_prefixes
-      destination_address_prefix = security_rule.value.destination_address_prefix
-      destination_address_prefixes = security_rule.value.destination_address_prefixes
+      name                                  = security_rule.key
+      priority                              = security_rule.value.priority
+      direction                             = security_rule.value.direction
+      access                                = security_rule.value.access
+      protocol                              = security_rule.value.protocol
+      source_port_range                     = security_rule.value.source_port_range
+      source_port_ranges                    = security_rule.value.source_port_ranges
+      destination_port_range                = security_rule.value.destination_port_range
+      destination_port_ranges               = security_rule.value.destination_port_ranges
+      source_address_prefix                 = security_rule.value.source_address_prefix
+      source_address_prefixes               = security_rule.value.source_address_prefixes
+      destination_address_prefix            = security_rule.value.destination_address_prefix
+      destination_address_prefixes          = security_rule.value.destination_address_prefixes
       source_application_security_group_ids = security_rule.value.source_application_security_group_ids
     }
 
