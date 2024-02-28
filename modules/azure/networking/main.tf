b8b53708a129dfe93fa0b359c50d307e4f22c0af
Ian Koome <47313708+KoomeKiriinya@users.noreply.github.com>
2024-02-28 17:27:19 +0300

    adding network security groups to azure networking module (#30)
    * add nsgs to vnet module
    
    * set correct variable for location & resource_group
    
    * set correct subnet reference
    
    * adding other arguments to security rules
    
    * adding other arguments to security rules
    
    * set condition to be based on length of security_rules

--- modules/azure/networking/main.tf
+++ modules/azure/networking/main.tf
@@ -59,3 +59,40 @@ resource "azurerm_subnet_nat_gateway_association" "subnet_nat_gateway_associatio
   subnet_id      = azurerm_subnet.subnets[each.key].id
   nat_gateway_id = azurerm_nat_gateway.nat_gateway.id
 }
+
+resource "azurerm_network_security_group" "security_groups" {
+  for_each = { for subnet, subnet-details in var.subnets :
+  subnet => subnet-details if length(subnet-details.security_rules) > 0 }
+  name                = "${each.key}-NSG"
+  location            = var.vnet_location
+  resource_group_name = var.resource_group_name
+
+  dynamic "security_rule" {
+    for_each = each.value.security_rules
+
+    content {
+      name                                  = security_rule.key
+      priority                              = security_rule.value.priority
+      direction                             = security_rule.value.direction
+      access                                = security_rule.value.access
+      protocol                              = security_rule.value.protocol
+      source_port_range                     = security_rule.value.source_port_range
+      source_port_ranges                    = security_rule.value.source_port_ranges
+      destination_port_range                = security_rule.value.destination_port_range
+      destination_port_ranges               = security_rule.value.destination_port_ranges
+      source_address_prefix                 = security_rule.value.source_address_prefix
+      source_address_prefixes               = security_rule.value.source_address_prefixes
+      destination_address_prefix            = security_rule.value.destination_address_prefix
+      destination_address_prefixes          = security_rule.value.destination_address_prefixes
+      source_application_security_group_ids = security_rule.value.source_application_security_group_ids
+    }
+
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "subnet_security_groups_association" {
+  for_each = { for subnet, subnet-details in var.subnets :
+  subnet => subnet-details if length(subnet-details.security_rules) > 0 }
+  subnet_id                 = azurerm_subnet.subnets[each.key].id
+  network_security_group_id = azurerm_network_security_group.security_groups[each.key].id
+}
--- modules/azure/networking/variables.tf
+++ modules/azure/networking/variables.tf
@@ -34,5 +34,20 @@ variable "subnets" {
       service_delegation_name    = string
       service_delegation_actions = list(string)
     }))
+    security_rules = optional(map(object({
+      priority                              = number
+      direction                             = string
+      access                                = string
+      protocol                              = string
+      source_port_range                     = optional(string)
+      source_port_ranges                    = optional(list(string))
+      destination_port_range                = optional(string)
+      destination_port_ranges               = optional(list(string))
+      source_address_prefix                 = optional(string)
+      source_address_prefixes               = optional(list(string))
+      destination_address_prefix            = optional(string)
+      destination_address_prefixes          = optional(list(string))
+      source_application_security_group_ids = optional(list(string))
+    })), {})
   }))
 }
