a661f5c235395dc899f3ddd56f8893cf740eb2ab
Ian Koome <47313708+KoomeKiriinya@users.noreply.github.com>
2024-04-03 10:57:02 +0300

    Update main.tf

--- modules/azure/vpn/main.tf
+++ modules/azure/vpn/main.tf
@@ -18,7 +18,7 @@ resource "azurerm_public_ip" "gw" {
   location            = var.location
   resource_group_name = var.resource_group_name
 
-  allocation_method = "Static"
+  allocation_method = var.sku == "Basic" ? "Dynamic" : "Static"
   sku               = var.sku == "Basic" ? "Basic" : "Standard"
 }
 
