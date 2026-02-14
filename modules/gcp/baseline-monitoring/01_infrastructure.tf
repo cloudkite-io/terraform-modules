94aa2b581c236513e31f8495be9feb6ac2c67e82
adeniyistephen <adeniyistadz@gmail.com>
2026-02-14 20:35:27 +0100

    made adjustment to infra dashborad

--- modules/gcp/baseline-monitoring/01_infrastructure.tf
+++ modules/gcp/baseline-monitoring/01_infrastructure.tf
@@ -26,121 +26,177 @@ resource "google_monitoring_dashboard" "infrastructure_dashboard" {
     {
       "filterType": "RESOURCE_LABEL",
       "labelKey": "project_id",
-      "templateVariable": "project_id",
-      "stringValue": ""
+      "templateVariable": "project_id"
+    },
+    {
+      "filterType": "RESOURCE_LABEL",
+      "labelKey": "cluster_name",
+      "templateVariable": "cluster_name"
     }
   ],
   "gridLayout": {
     "columns": "2",
     "widgets": [
       {
-        "title": "Total Container Restarts",
+        "title": "Unhealthy Nodes",
         "scorecard": {
           "timeSeriesQuery": {
             "timeSeriesFilter": {
-              "filter": "metric.type=\"kubernetes.io/container/restart_count\" resource.type=\"k8s_container\"",
+              "filter": "metric.type=\"kubernetes.io/node/status_condition\" resource.type=\"k8s_node\" metric.label.condition=\"Ready\" value.condition_state=\"false\"",
               "aggregation": {
-                "perSeriesAligner": "ALIGN_DELTA",
-                "crossSeriesReducer": "REDUCE_SUM",
-                "alignmentPeriod": "3600s"
+                "perSeriesAligner": "ALIGN_NEXT_OLDER",
+                "crossSeriesReducer": "REDUCE_COUNT",
+                "alignmentPeriod": "60s"
               }
             },
-            "unitOverride": "Restarts"
+            "unitOverride": "Nodes"
           }
         }
       },
       {
-        "title": "Total OOM Crashes",
+        "title": "Total Healthy Nodes",
         "scorecard": {
           "timeSeriesQuery": {
             "timeSeriesFilter": {
-              "filter": "metric.type=\"logging.googleapis.com/user/k8s_oom_events\" resource.type=\"k8s_container\"",
+              "filter": "metric.type=\"kubernetes.io/node/status_condition\" resource.type=\"k8s_node\" metric.label.condition=\"Ready\" value.condition_state=\"true\"",
               "aggregation": {
-                "perSeriesAligner": "ALIGN_DELTA",
-                "crossSeriesReducer": "REDUCE_SUM",
-                "alignmentPeriod": "3600s"
+                "perSeriesAligner": "ALIGN_NEXT_OLDER",
+                "crossSeriesReducer": "REDUCE_COUNT",
+                "alignmentPeriod": "60s"
               }
             },
-            "unitOverride": "Crashes"
+            "unitOverride": "Nodes"
           }
         }
       },
       {
-        "title": "Top Nodes by CPU Utilization",
-        "xyChart": {
-          "dataSets": [{
-            "timeSeriesQuery": {
-              "timeSeriesFilter": {
-                "filter": "metric.type=\"kubernetes.io/node/cpu/allocatable_utilization\" resource.type=\"k8s_node\"",
-                "aggregation": {
-                  "perSeriesAligner": "ALIGN_MEAN",
-                  "crossSeriesReducer": "REDUCE_MEAN",
-                  "groupByFields": ["resource.label.node_name"]
-                }
-              }
-            },
-            "plotType": "LINE"
-          }],
-          "chartOptions": {
-            "mode": "COLOR"
-          }
-        }
-      },
-      {
-        "title": "Top Nodes by Memory Utilization",
-        "xyChart": {
-          "dataSets": [{
-            "timeSeriesQuery": {
-              "timeSeriesFilter": {
-                "filter": "metric.type=\"kubernetes.io/node/memory/allocatable_utilization\" resource.type=\"k8s_node\"",
-                "aggregation": {
-                  "perSeriesAligner": "ALIGN_MEAN",
-                  "crossSeriesReducer": "REDUCE_MEAN",
-                  "groupByFields": ["resource.label.node_name"]
-                }
-              }
-            },
-            "plotType": "LINE"
-          }]
-        }
-      },
-      {
-        "title": "Top Fullest Disks (>80%)",
-        "xyChart": {
-          "dataSets": [{
-            "timeSeriesQuery": {
-              "timeSeriesFilter": {
-                "filter": "metric.type=\"kubernetes.io/node/ephemeral_storage/used_bytes\" resource.type=\"k8s_node\"",
-                "aggregation": {
-                  "perSeriesAligner": "ALIGN_MEAN",
-                  "crossSeriesReducer": "REDUCE_MEAN",
-                  "groupByFields": ["resource.label.node_name"]
-                }
-              }
-            },
-            "plotType": "STACKED_BAR"
-          }]
-        }
-      },
-      {
-        "title": "Container Restarts (Count by Pod)",
+        "title": "Top Restarting Containers (By Pod Name)",
         "xyChart": {
           "dataSets": [{
             "timeSeriesQuery": {
               "timeSeriesFilter": {
                 "filter": "metric.type=\"kubernetes.io/container/restart_count\" resource.type=\"k8s_container\"",
                 "aggregation": {
-                  "perSeriesAligner": "ALIGN_DELTA", 
+                  "perSeriesAligner": "ALIGN_DELTA",
                   "crossSeriesReducer": "REDUCE_SUM",
-                  "groupByFields": ["resource.label.pod_name", "resource.label.namespace_name"]
+                  "groupByFields": ["resource.label.namespace_name", "resource.label.pod_name"]
                 }
               }
             },
             "plotType": "STACKED_BAR"
           }],
           "yAxis": {
-            "label": "Count",
-            "scale": "LINEAR"
+             "label": "Restarts",
+             "scale": "LINEAR"
+          },
+          "chartOptions": {
+            "mode": "COLOR"
+          }
+        }
+      },
+      {
+        "title": "Top OOM Crashes (By Pod Name)",
+        "xyChart": {
+          "dataSets": [{
+            "timeSeriesQuery": {
+              "timeSeriesFilter": {
+                "filter": "metric.type=\"logging.googleapis.com/user/k8s_oom_events\" resource.type=\"k8s_container\"",
+                "aggregation": {
+                  "perSeriesAligner": "ALIGN_DELTA",
+                  "crossSeriesReducer": "REDUCE_SUM",
+                  "groupByFields": ["resource.label.namespace_name", "resource.label.pod_name"]
+                }
+              }
+            },
+            "plotType": "STACKED_BAR"
+          }],
+           "yAxis": {
+             "label": "OOM Count",
+             "scale": "LINEAR"
+          },
+          "chartOptions": {
+            "mode": "COLOR"
+          }
+        }
+      },
+      {
+        "title": "Disk Capacity & Usage (Table)",
+        "collapsibleGroup": {
+          "collapsed": false
+        },
+        "text": {
+          "content": "This table shows the current Used vs Total capacity per node.",
+          "format": "MARKDOWN"
+        }
+      },
+      {
+        "title": "Disk Usage Table",
+        "table": {
+          "dataSets": [
+            {
+              "timeSeriesQuery": {
+                "timeSeriesFilter": {
+                  "filter": "metric.type=\"kubernetes.io/node/ephemeral_storage/used_bytes\" resource.type=\"k8s_node\"",
+                  "aggregation": {
+                    "perSeriesAligner": "ALIGN_NEXT_OLDER",
+                    "crossSeriesReducer": "REDUCE_LAST",
+                    "groupByFields": ["resource.label.node_name"]
+                  }
+                }
+              },
+              "tableTemplate": "Used Bytes"
+            },
+            {
+              "timeSeriesQuery": {
+                "timeSeriesFilter": {
+                  "filter": "metric.type=\"kubernetes.io/node/ephemeral_storage/allocatable_bytes\" resource.type=\"k8s_node\"",
+                  "aggregation": {
+                    "perSeriesAligner": "ALIGN_NEXT_OLDER",
+                    "crossSeriesReducer": "REDUCE_LAST",
+                    "groupByFields": ["resource.label.node_name"]
+                  }
+                }
+              },
+              "tableTemplate": "Total Capacity"
+            }
+          ]
+        }
+      },
+      {
+        "title": "Disk IOPS (Read vs Write)",
+        "xyChart": {
+          "dataSets": [
+            {
+              "timeSeriesQuery": {
+                "timeSeriesFilter": {
+                  "filter": "metric.type=\"compute.googleapis.com/instance/disk/read_ops_count\" resource.type=\"gce_instance\"",
+                  "aggregation": {
+                    "perSeriesAligner": "ALIGN_RATE",
+                    "crossSeriesReducer": "REDUCE_SUM"
+                  }
+                },
+                "unitOverride": "Read Ops"
+              },
+              "plotType": "LINE",
+              "legendTemplate": "Read IOPS"
+            },
+            {
+              "timeSeriesQuery": {
+                "timeSeriesFilter": {
+                  "filter": "metric.type=\"compute.googleapis.com/instance/disk/write_ops_count\" resource.type=\"gce_instance\"",
+                  "aggregation": {
+                    "perSeriesAligner": "ALIGN_RATE",
+                    "crossSeriesReducer": "REDUCE_SUM"
+                  }
+                },
+                "unitOverride": "Write Ops"
+              },
+              "plotType": "LINE",
+              "legendTemplate": "Write IOPS"
+            }
+          ],
+          "chartOptions": {
+            "mode": "COLOR"
           }
         }
       }
